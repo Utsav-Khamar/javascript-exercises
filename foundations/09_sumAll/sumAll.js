d5f3b7075de0109d79bc919dd498b10417629166
Utsav-Khamar <utsavkhamar2@gmail.com>
2026-09-10 00:42:04 -0400

    Solve javascript excercise-9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+//this is not an elegant solution as I couldn't remember isInteger method at time. 
+const sumAll = function (start, end) {
+    //check if start and end are number
+    if (typeof (start) === 'number' && typeof (end) === 'number') {
+        //check if start and end are integers and greater than 0
+        if ((start >= 0 && Math.floor(start) === start)
+            && (end >= 0 && Math.floor(end) === end)) {
+            //check which one is bigger
+            if (start > end) {
+                [start, end] = [end, start];
+            }
+
+            let sum = 0;
+            while (start <= end) {
+                sum += start;
+                start++;
+            }
+            return sum;
+        }
+    }
+    return "ERROR";
 
 };
 
