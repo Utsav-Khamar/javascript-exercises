3f2a96c81d1a6f18eab9351f87a19715905f571f
Utsav-Khamar <utsavkhamar2@gmail.com>
2026-09-10 00:13:28 -0400

    Solve javascript excercise-8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+    let newArr = [];
+    for (ele of array) {
+        if (items.includes(ele)) {
+            continue;
+        }
+        newArr.push(ele);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
